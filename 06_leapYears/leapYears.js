183560025ff749be9f54d07db2b304572b88a3d9
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-25 23:09:37 -0500

    Pass 06_leapYears test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function (number) {
+    if (!Number.isInteger(number) || number < 0) {
+        return "wrong Input";
+    }
+    if (number % 4 === 0) {
+        if (number % 100 !== 0) {
+            return true;
+        } else {
+            if (number % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
