6dcb9ec78dbe7f0fd8c8c587c2714d60b5f99aac
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-25 21:53:27 -0500

    Pass 02_repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    if (string === "" || number === 0) {
+        return "";
+    }
+    if (number < 0) {
+        return "ERROR";
+    }
+    let finalString = "";
+    for (let i = 1; i <= number; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
