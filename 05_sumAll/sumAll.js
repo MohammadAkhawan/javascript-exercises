cf1631c33b178a81f5cfdb7b7f771cd6b4a8619b
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-25 22:51:40 -0500

    Pass 05_sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (
+        typeof num1 !== "number" ||
+        typeof num2 !== "number" ||
+        num1 < 0 ||
+        num2 < 0
+    ) {
+        return "ERROR";
+    }
 
+    if (num1 === num2) {
+        return num1;
+    }
+
+    let smallNumber;
+    let largeNumber;
+    let finalNumber = 0;
+
+    if (num1 > num2) {
+        largeNumber = num1;
+        smallNumber = num2;
+    } else {
+        largeNumber = num2;
+        smallNumber = num1;
+    }
+
+    while (smallNumber <= largeNumber) {
+        finalNumber += smallNumber;
+        smallNumber++;
+    }
+
+    return finalNumber;
 };
 
 // Do not edit below this line
