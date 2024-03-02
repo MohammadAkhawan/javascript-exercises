40d8513658f1c0e664ddc61b0bca753888539cda
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-03-01 23:34:37 -0500

    Pass 10_fibonacci test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (Number(num) === 0) return 0;
+    if (Number(num) === 1) return 1;
+    if (Number(num) < 0) return "OOPS";
+    let counter = Number(num);
+    let num1 = 0;
+    let num2 = 1;
+    let temp;
+    while (counter > 1) {
+        temp = num2;
+        num2 += num1;
+        num1 = temp;
+        counter--;
+    }
+    return num2;
 };
 
 // Do not edit below this line
