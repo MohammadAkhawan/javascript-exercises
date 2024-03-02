5b8046b9f35850c9be8e174dbfb72af3a6946e9f
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-03-01 23:07:38 -0500

    Pass 09_palindromes test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const checkString = string.toLowerCase().replace(/[^a-z0-9]/g,"").split("").reverse().join("");
+    return string.toLowerCase().replace(/[^a-z0-9]/g,"") === checkString;
 };
 
 // Do not edit below this line
