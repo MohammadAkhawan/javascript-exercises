be99165cbcedd0ba42046c931b746a4bf1aac163
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-03-01 22:15:04 -0500

    Pass 09_calculator test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((sum, currentNumber) => sum + currentNumber, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+    return arr.reduce((total, currentNumber) => total * currentNumber);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    const initialA = a;
+    if (b === 0) return 1;
+    else if (b === 1) return a;
+    while (b > 1) {
+        a *= initialA;
+        b--;
+    }
+    return a;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0) return 1;
+    let fact = 1;
+    while (num > 1) {
+        fact *= num;
+        num--;
+    }
+    return fact;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
