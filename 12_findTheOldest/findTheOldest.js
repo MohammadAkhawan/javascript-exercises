65e8408208f5f43767d12e123357721ba241d736
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-03-02 22:30:00 -0500

    Pass 12_findTheOldest test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let oldAge = 0;
+    let index;
+    for (const person of people) {
+        if (person["yearOfDeath"] && person["yearOfBirth"]) {
+            const age = person["yearOfDeath"] - person["yearOfBirth"];
+            if (age > oldAge) {
+                oldAge = age;
+                index = people.indexOf(person);
+            }
+        } else if (person["yearOfBirth"]) {
+            const age = 2024 - person["yearOfBirth"];
+            if (age > oldAge) {
+                oldAge = age;
+                index = people.indexOf(person);
+            }
+        }
+    }
+    return people[index];
 };
 
 // Do not edit below this line
